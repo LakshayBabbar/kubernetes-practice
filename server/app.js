7c7ce756f3b2ac1a8caa6e0090534606be571038
lakshaybabbar <lakshaybabbar0118@gmail.com>
2024-08-05 13:03:18 +0530

    added files

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import bodyParser from "body-parser";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import { router } from "./routes/routes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(router);
+
+app.get("/", (req, res) => {
+  try {
+    return res.status(200).json({ message: "Hello World" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  connectDB();
+});
